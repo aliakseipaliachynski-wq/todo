54053e273c742e556b655228086f19e9b4749eb3
Aliaksei <Aliaksei>
2026-02-04 21:27:00 +0300

    Update package.json to change husky installation command

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,7 +5,9 @@ const cryptoShim = { randomUUID } as Crypto;
 if (typeof globalThis.crypto?.randomUUID !== "function") {
   globalThis.crypto = cryptoShim;
 }
-if (typeof global !== "undefined" && typeof (global as NodeJS.Global & { crypto?: Crypto }).crypto?.randomUUID !== "function") {
-  (global as NodeJS.Global & { crypto: Crypto }).crypto = cryptoShim;
+if (
+  typeof global !== "undefined" &&
+  typeof (global as { crypto?: Crypto }).crypto?.randomUUID !== "function"
+) {
+  (global as { crypto: Crypto }).crypto = cryptoShim;
 }
-
